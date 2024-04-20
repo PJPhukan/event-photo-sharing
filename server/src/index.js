9d510b5e6db16718842bb831a4a96093aa5b82b9
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-04-20 23:04:22 +0530

    feat : " Express setup completed "

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+import { app } from "./app.js";
+import { MongoConnect } from "./db/index.js";
+
+const port = process.env.PORT || 3000;
+
+// TODO INSTALL CORS , COOKIE-PARSER
+
+MongoConnect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(
+        `♦️♦️ Memois web application running on  http://localhost:${port} ♦️♦️ `
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("MONGO CONNECTION ERROR !!! ", error);
+  });
