d205017913778f28bff4caf641a5279d2e4dacee
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-06-09 22:38:19 +0530

    Add some functionality in Create event page

--- client/src/page/CreateEvent/CreateEvent.jsx
+++ client/src/page/CreateEvent/CreateEvent.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import EventCard from "../../component/EventCard/EventCard";
-import './createevent.scss'
+import "./createevent.scss";
+import { Link } from "react-router-dom";
 const CreateEvent = () => {
   return (
     <section className="create-event-main">
+      <div className="top-event">
+        <div className="section-heading">
+          <div className="main-heading">Events</div>
+          <div className="sub-heading">Manage your all events</div>
+        </div>
+        <Link to="/dashboard/create-event" className="create-event-btn"> Create Event</Link>
+      </div>
       <div className="event-main-content">
         <EventCard />
         <EventCard />
