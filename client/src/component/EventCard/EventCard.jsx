8473c441548472b4a38fed75bfc75e0781d88d0d
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-07-17 22:01:21 +0530

    Event card css completed

--- client/src/component/EventCard/EventCard.jsx
+++ client/src/component/EventCard/EventCard.jsx
@@ -2,99 +2,93 @@ import React, { useState, useRef, useEffect } from "react";
 import "./eventcard.scss";
 import { GiLoveLetter } from "react-icons/gi";
 import { Link } from "react-router-dom";
+import ProfileImg from "../../assets/collLogo.png";
+import { MdDelete } from "react-icons/md";
 const EventCard = () => {
-  const [isHovered, setIsHovered] = useState(false);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const cardRef = useRef(null);
-  const [bounds, setBounds] = useState({ x: 0, y: 0, width: 0, height: 0 });
+  const [showEventPopUP, setshowEventPopUP] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-    if (cardRef.current) {
-      setBounds(cardRef.current.getBoundingClientRect());
-    }
+  //TODO:Edit Details of the event
+  const EditDetailsEvent = () => {
+    console.log("Edit Details button was clicked");
   };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-    setMousePosition({ x: 0, y: 0 });
+  //TODO:Change avatar of the event
+  const ChangeAvatarEvent = () => {
+    console.log("Change Avatar Event button was clicked");
   };
 
-  const handleMouseMove = (e) => {
-    setMousePosition({ x: e.clientX, y: e.clientY });
+  //TODO:Delete event
+  const DeleteEvent = () => {
+    console.log("Delete Event button was clicked");
   };
-
-  const getTransformStyles = () => {
-    if (!isHovered) return {};
-    const leftX = mousePosition.x - bounds.x;
-    const topY = mousePosition.y - bounds.y;
-    const center = {
-      x: leftX - bounds.width / 2,
-      y: topY - bounds.height / 2,
-    };
-    const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
-
-    return {
-      transform: `
-        scale3d(1.07, 1.07, 1.07)
-        rotate3d(
-          ${center.y / 100},
-          ${-center.x / 100},
-          0,
-          ${Math.log(distance) * 2}deg
-        )
-      `,
-      backgroundImage: `
-        radial-gradient(
-          circle at
-          ${center.x * 2 + bounds.width / 2}px
-          ${center.y * 2 + bounds.height / 2}px,
-          #ffffff55,
-          #0000000f
-        )
-      `,
-    };
-  };
-
-  useEffect(() => {
-    if (isHovered && cardRef.current) {
-      setBounds(cardRef.current.getBoundingClientRect());
-    }
-  }, [isHovered]);
-
   return (
-    <div
-      className="event-card"
-      ref={cardRef}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onMouseMove={handleMouseMove}
-      style={getTransformStyles()}
-    >
-      <div className="event-card-box">
-        <Link to="/create-event" className="top">
-          <div className="left">
-            <div className="title">College</div>
-            <div className="top-buttom">
-              <div className="amount">50</div>
-              <div className="buttom-text">Photos & Videos</div>
-            </div>
-          </div>
-          <div className="right">
-            <GiLoveLetter />
-          </div>
-        </Link>
-        <div className="buttom">
-          <div className="date">12 June, 2024</div>
-          <div className="edit">
-            <i
-              className="bx bx-dots-vertical-rounded"
-              onClick={() => console.log("Click on icon")}
-            ></i>
-          </div>
+    <div className="event-card">
+      <div className="top-box">
+        <img src={ProfileImg} alt="" className="event-logo" />
+        <div className="top-text">
+          <h4 className="event-name">Event name</h4>
+          <h5 className="event-type">Marrage</h5>
         </div>
       </div>
-      <div className="glow" />
+      <p className="event-description">
+        Discover a curated collection of stunning images perfect for enhancing
+        your creative endeavors. High-quality, versatile, and ready to inspire.
+      </p>
+
+      <div className="details-box">
+        <div className="contact-details-item">
+          <i className="bx bx-map"></i>
+          <span>Maligoan, Guwahati, Assam, India</span>
+        </div>
+        <div className="contact-details-item">
+          <i className="bx bx-calendar"></i>
+          <span>12 pm , 25 June ,2024</span>
+        </div>
+        <div className="contact-details-item">
+          <i className="bx bx-phone"></i>
+          <span>
+            <span className="country-code">+91</span>
+            <span>7002925194</span>
+          </span>
+        </div>
+      </div>
+      <div className="time-details">
+        <p className="created-time">
+          Created was <span className="show-time"> 12:00 pm</span>, 12 Jun ,
+          2024
+        </p>
+        <button
+          className="view-more"
+          onClick={() => setshowEventPopUP(!showEventPopUP)}
+        >
+          <i className="bx bx-dots-vertical"></i>
+        </button>
+
+        <div
+          className={`pop-up-model ${
+            showEventPopUP ? "active-event-pop-up" : ""
+          }`}
+        >
+          <button
+            className="cross-icon"
+            onClick={() => setshowEventPopUP(false)}
+          >
+            <i className="bx bx-x"></i>
+          </button>
+          <button className="pop-up-item" onClick={EditDetailsEvent}>
+            <i className="bx bx-edit-alt"></i>
+            <span>Edit Details</span>
+          </button>
+          <button className="pop-up-item" onClick={ChangeAvatarEvent}>
+            <i className="bx bxs-camera"></i>
+            <span>Change Avatar</span>
+          </button>
+          <button className="pop-up-item" onClick={DeleteEvent}>
+            <MdDelete />
+            <span>Delete Event</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
