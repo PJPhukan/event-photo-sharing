e7dc76bdeebf9bf6c66d6091fd12c02a64cc3281
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-07-25 00:00:14 +0530

    add event and like routes

---
+++ server/src/route/like.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { NewLike, DeleteLike } from "../controller/like.controllers.js";
+import { VerifyJwtToken } from "../libs/auth.helpers.js";
+const router = Router();
+
+router.route("/like-post").post(VerifyJwtToken, NewLike); //user logged in required
+
+router.route("/unlike-post").delete(VerifyJwtToken, DeleteLike); //user logged in required
+
+export default router;
