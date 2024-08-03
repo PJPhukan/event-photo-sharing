8f132bad533883ce3667ced3d25ab76a940ffaf1
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-03 20:14:34 +0530

    Finished up event section

--- client/src/page/Event/Event.jsx
+++ client/src/page/Event/Event.jsx
@@ -1,10 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import EventCard from "../../component/EventCard/EventCard";
 import "./event.scss";
-import {context} from "../../../Context/context";
+import { context } from "../../../Context/context";
+import { dashboad } from "../../../Context/context";
 const CreateEvent = () => {
   const userContext = useContext(context);
   const { setcreateEvent } = userContext;
+  const dashboardContext = useContext(dashboad);
+  const { get_event } = dashboardContext;
+  const [events, setevents] = useState([]);
+  const handle_event = async () => {
+    const res = await get_event();
+    setevents(res.data.data);
+  };
+  useEffect(() => {
+    handle_event();
+  });
+
   return (
     <section className="event-main">
       <div className="top-event">
@@ -16,16 +28,14 @@ const CreateEvent = () => {
           className="create-event-btn"
           onClick={() => setcreateEvent(true)}
         >
-          {" "}
           Create Event
         </button>
       </div>
       <div className="event-main-content">
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
+        {events.map((event) => {
+          return <EventCard key={event._id} data={event}/>;
+        })}
+       
       </div>
     </section>
   );
