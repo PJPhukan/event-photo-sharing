1ad9a0107ea939e436a9fb21920aaba7d9a045f6
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-03 20:19:15 +0530

    Change image model and route

--- server/src/route/image.route.js
+++ server/src/route/image.route.js
@@ -5,11 +5,11 @@ import { VerifyJwtToken } from "../libs/auth.helpers.js";
 const router = Router();
 
 router
-  .route("/upload-image")
+  .route("/upload-image/:eventId")
   .post(VerifyJwtToken, upload.array("image", 10), UploadImage); //user logged in required
 
-router.route("/delete-image").delete(VerifyJwtToken, DeleteImage); // logged in required
+router.route("/delete-image/:imageId").delete(VerifyJwtToken, DeleteImage); // logged in required
 
-router.route("/get-image").get(VerifyJwtToken, GetImage); // logged in required
+router.route("/get-image/:imageId").get(VerifyJwtToken, GetImage); // logged in required
 
 export default router;
