656c35b5c7f05cfdefc88ebc2d44144f971c4e88
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-03 20:07:05 +0530

    Connect with database and fixed css

--- client/src/component/EventCard/EventCard.jsx
+++ client/src/component/EventCard/EventCard.jsx
@@ -4,18 +4,22 @@ import { Link } from "react-router-dom";
 import ProfileImg from "../../assets/collLogo.png";
 import { MdDelete } from "react-icons/md";
 import { FaRegEye } from "react-icons/fa";
-import {context} from "../../../Context/context";
-const EventCard = () => {
+import { context } from "../../../Context/context";
+import { dashboad } from "../../../Context/context";
+const EventCard = ({ data }) => {
   const [showEventPopUP, setshowEventPopUP] = useState(false);
   const { seteditEvent, downloadQR, setdownloadQR } = useContext(context);
+  const DashboardContext = useContext(dashboad);
+  const { delete_event, setqrtext } = DashboardContext;
+
   //TODO:Edit Details of the event
   const EditDetailsEvent = () => {
     console.log("Edit Details button was clicked");
   };
 
-  //TODO:Delete event
-  const DeleteEvent = () => {
-    console.log("Delete Event button was clicked");
+  const DeleteEvent = async () => {
+    const res = await delete_event(data._id);
+    // console.log(res.data.success);
   };
 
   const getSubstring = (str, len) => {
@@ -24,51 +28,73 @@ const EventCard = () => {
     }
     return str;
   };
+
+  const returnTimeDate = (time) => {
+    const date = new Date(time);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    };
+    const formattedDate = date.toLocaleString("en-US", options);
+    const [datePart, timePart] = formattedDate.split(", ");
+    const [hour, minute] = timePart.split(":");
+    const customFormattedDate = `${hour}:${minute}, ${datePart}`;
+    return customFormattedDate;
+  };
+
+  const handleQRCode = () => {
+    setqrtext(`http://localhost:5173/event/${data.user_id}/${data._id}`);
+    setdownloadQR(true);
+  };
   return (
     <div className="event-card">
       <div className="top-box">
         <div className="event-card-box">
-          <img src={ProfileImg} alt="" className="event-logo" />
+          <img
+            src={data.CoverImage ? data.CoverImage : ProfileImg}
+            alt=""
+            className="event-logo"
+          />
           <div className="top-text">
             <h4 className="event-name">
-              {getSubstring("College annual function", 10)}
+              {getSubstring(`${data.EventName}`, 10)}
             </h4>
-            <h5 className="event-type">Marrage</h5>
+            <h5 className="event-type">{data.EventType}</h5>
           </div>
         </div>
-        <Link to="/dashboard/event/2" className="btn-view">
+        <Link to={`/dashboard/event/${data._id}`} className="btn-view">
           <FaRegEye />
           View
         </Link>
       </div>
       <p className="event-description">
-        {getSubstring(
-          "Discover a curated collection of stunning images perfect for enhancingyour creative endeavors. High-quality, versatile, and ready to inspire.",
-          50
-        )}
+        {getSubstring(`${data.EventDescription}`, 50)}
       </p>
 
       <div className="details-box">
         <div className="contact-details-item">
           <i className="bx bx-map"></i>
-          <span>Maligoan, Guwahati, Assam, India</span>
+          <span>{data.EventLocation}</span>
         </div>
         <div className="contact-details-item">
           <i className="bx bx-calendar"></i>
-          <span>12 pm , 25 June ,2024</span>
+          <span>{returnTimeDate(data.EventDate)}</span>
         </div>
         <div className="contact-details-item">
           <i className="bx bx-phone"></i>
           <span>
             <span className="country-code">+91</span>
-            <span>7002925194</span>
+            <span>{data.ContactDetails}</span>
           </span>
         </div>
       </div>
       <div className="time-details">
         <p className="created-time">
-          Created was <span className="show-time"> 12:00 pm</span>, 12 Jun ,
-          2024
+          Created was {returnTimeDate(data.createdAt)}
         </p>
         <button
           className="view-more"
@@ -92,7 +118,7 @@ const EventCard = () => {
             <i className="bx bx-edit-alt"></i>
             <span>Edit Details</span>
           </button>
-          <button className="pop-up-item" onClick={() => setdownloadQR(true)}>
+          <button className="pop-up-item" onClick={handleQRCode}>
             <i className="bx bx-qr"></i>
             <span>QR Code</span>
           </button>
