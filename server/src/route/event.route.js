b2f4ef9c8189d0e0a3c21e47490503ad64046e05
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-03 20:20:10 +0530

    change route params

--- server/src/route/event.route.js
+++ server/src/route/event.route.js
@@ -12,12 +12,12 @@ const router = Router();
 
 router.route("/create-event").post(VerifyJwtToken, CreateEvent); //logged in required
 
-router.route("/delete-event").delete(VerifyJwtToken, DeleteEvent); //user logged in required
+router.route("/delete-event/:eventId").delete(VerifyJwtToken, DeleteEvent); //user logged in required
 
-router.route("/edit-event").patch(VerifyJwtToken, EditEvent); //logged in required
+router.route("/edit-event/:eventId").patch(VerifyJwtToken, EditEvent); //logged in required
 
 router.route("/get-events").get(VerifyJwtToken, GetAllEvent); //logged in required
 
-router.route("/get-event-details").get(VerifyJwtToken, EventDetails); //logged in required
+router.route("/get-event-details/:eventId").get(VerifyJwtToken, EventDetails); //logged in required
 
 export default router;
