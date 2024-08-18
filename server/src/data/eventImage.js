712c80be5f0fad50f6a4cb87effce53767e9f6d9
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-18 10:27:04 +0530

    Update eventimage data by cloudinary url

--- server/src/data/eventImage.js
+++ server/src/data/eventImage.js
@@ -1,31 +1,31 @@
 const image = [
   {
     type: "wedding",
-    url: "./Images/collLogo.png",
+    url: "https://res.cloudinary.com/pjphukan/image/upload/v1723955826/kt7r8ev5hyjhtuivnn86.png",
   },
   {
     type: "convocation",
-    url: "./Images/convocation.webp",
+    url: "https://res.cloudinary.com/pjphukan/image/upload/v1723956442/njyeokzxgawaswd1qihz.webp",
   },
   {
     type: "Marathon",
-    url: "./Images/sports.jpg",
+    url: "https://res.cloudinary.com/pjphukan/image/upload/v1723956488/ivusrngvhebchb9a6nki.jpg",
   },
   {
     type: "schools",
-    url: "./Images/collegeSchool.jpg",
+    url: "https://res.cloudinary.com/pjphukan/image/upload/v1723956328/gxzmtjipt49a97qlhxdg.jpg",
   },
   {
     type: "College",
-    url: "./Images/R.jpeg",
+    url: "https://res.cloudinary.com/pjphukan/image/upload/v1723956540/xx2cqhwkeimdrtsuxmqa.jpg",
   },
   {
     type: "Social Club",
-    url: "./Images/clubs.png",
+    url: "https://res.cloudinary.com/pjphukan/image/upload/v1723956640/ws6b2l1b4k7bm70uqgoi.png",
   },
   {
     type: "Corporate Event",
-    url: "./Images/corporate.jpeg",
+    url: "https://res.cloudinary.com/pjphukan/image/upload/v1723956725/dzk4iwcsedewcwsgyvtn.jpg",
   },
 ];
 export { image };
